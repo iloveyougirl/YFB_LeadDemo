82955343b7c8407806d239f355fcecd0dabbca57
PC201610101509\Administrator <392562208@qq.com>
2016-11-08 13:07:45 +0800

    no message

---
+++ my-project-home/src/components/cleancash.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>{{mes}}</div>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+            id:'',
+            mes:''
+            }
+        },
+        components:{
+        },
+        ready:function(){
+          this.$http.get('/cache/flush')
+                   .then((response) => {
+                   this.$set('mes', response.data.msg)
+                    })
+                   .catch(function(response) {
+                       console.log(response)
+                   })
+       }
+       //else{
+     //  this.sendParameter()
+     //  }
+    }
+</script>
+<style>
+</style>
